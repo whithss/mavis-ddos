6921780543f1178b5b794f51c4edfcc994a2328d
WHITE HORSE <155746314+whithss@users.noreply.github.com>
2024-01-11 22:03:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,35 @@ echo "Iniciando  instalação"
 sleep 1 
 pkg install python -y 
 clear
+echo " █▀▄▀█ █▀▀█ ▀█░█▀ ░▀░ █▀▀"
+echo " █▒█▒█ █▄▄█ ░█▄█░ ▀█▀ ▀▀█"
+echo " █░░▒█ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀"
+echo " █▀▀▄ █▀▀▄ █▀▀█ █▀▀"
+echo " █░░█ █░░█ █░░█ ▀▀█"
+echo " ▀▀▀░ ▀▀▀░ ▀▀▀▀ ▀▀▀"
 pip install colorama 
 clear
+echo " █▀▄▀█ █▀▀█ ▀█░█▀ ░▀░ █▀▀"
+echo " █▒█▒█ █▄▄█ ░█▄█░ ▀█▀ ▀▀█"
+echo " █░░▒█ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀"
+echo " █▀▀▄ █▀▀▄ █▀▀█ █▀▀"
+echo " █░░█ █░░█ █░░█ ▀▀█"
+echo " ▀▀▀░ ▀▀▀░ ▀▀▀▀ ▀▀▀"
 pip install tqdm 
 clear
+echo " █▀▄▀█ █▀▀█ ▀█░█▀ ░▀░ █▀▀"
+echo " █▒█▒█ █▄▄█ ░█▄█░ ▀█▀ ▀▀█"
+echo " █░░▒█ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀"
+echo " █▀▀▄ █▀▀▄ █▀▀█ █▀▀"
+echo " █░░█ █░░█ █░░█ ▀▀█"
+echo " ▀▀▀░ ▀▀▀░ ▀▀▀▀ ▀▀▀"
 pip install scapy
 clear
+echo " █▀▄▀█ █▀▀█ ▀█░█▀ ░▀░ █▀▀"
+echo " █▒█▒█ █▄▄█ ░█▄█░ ▀█▀ ▀▀█"
+echo " █░░▒█ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀"
+echo " █▀▀▄ █▀▀▄ █▀▀█ █▀▀"
+echo " █░░█ █░░█ █░░█ ▀▀█"
+echo " ▀▀▀░ ▀▀▀░ ▀▀▀▀ ▀▀▀"
+echo "by: White Horse"
 echo "Instalação completa"
